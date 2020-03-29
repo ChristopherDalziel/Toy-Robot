7480442bfddba9d8bc2c51d5fa6f0eca29450a9e
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-03-30 01:26:18 +1100

    Added east and west movements including tests

--- lib/toy_robot/robot.rb
+++ lib/toy_robot/robot.rb
@@ -1,16 +1,25 @@
 module ToyRobot
   class Robot
+    # This allows us to remove the position method below, it does the exact same thing, allows us to read the property.
+    attr_reader :position
+
     def initialize(position = 0)
       @position = position
     end
 
-    def move
+    def move_east
+      # Each time move is called, add 1 to the position
       @position += 1
     end
 
-    def position
-      @position
+    def move_west
+      @position -= 1
     end
+
+    # def position
+    #   # Return the value of position
+    #   @position
+    # end
 
   end
 end
