13d4a4fc704075dc70ad1ce5a8405e2721bab886
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-03-30 23:45:25 +1100

    began creating the table class to set the rules of the 5x5 grid/table/block

---
+++ lib/toy_robot/table.rb
@@ -0,0 +1,12 @@
+module ToyRobot
+  class Table
+    def initialize(width, length)
+      @width = width
+      @length = length
+    end
+
+    def valid_location?(east, north)
+    end
+
+  end
+end
