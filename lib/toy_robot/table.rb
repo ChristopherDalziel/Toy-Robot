058823cf0c9f97e227692652fcaf347b484fb91b
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-03-31 21:28:36 +1100

    Added logic and tests that govern the size of our table

--- lib/toy_robot/table.rb
+++ lib/toy_robot/table.rb
@@ -5,7 +5,15 @@ module ToyRobot
       @length = length
     end
 
+    # Logic: 
+    # Less than 0 = invalid
+    # More than or equal to c5 = invalid
     def valid_location?(east, north)
+      # This logic determines the width and length of our table
+      # east >= 0 && east < @width && north >= 0 && north < @length
+
+      # A cleaner way to determine our logic, the spread operator will exclude the value at the end of the range. In our case this is 0-4 (Removing 5). 
+      (0...@width).cover?(east) && (0...@length).cover?(north)
     end
 
   end
--- spec/toy_robot/table_spec.rb
+++ spec/toy_robot/table_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe ToyRobot::Table do
   context "valid_location?" do 
     it { should be_valid_location(0, 0) }
     it { should be_valid_location(4, 4) }
-    it { should be_valid_location(5, 5) }
-    it { should be_valid_location(-1, -1) }
+    it { should_not be_valid_location(5, 5) }
+    it { should_not be_valid_location(-1, -1) }
   end
 end
