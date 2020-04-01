748cdabb70af28115fefdd81490d2fc4a54147f0
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-04-01 22:20:32 +1100

    fixed command test file

--- spec/toy_robot/command_spec.rb
+++ spec/toy_robot/command_spec.rb
@@ -1,10 +1,11 @@
-require "spec_helper"
+require 'spec_helper'
 
-RSpec.describe ToyRobot::Command do
+RSpec.describe ToyRobot::Command do 
   context "PLACE" do
-    it "processes a PLACE command"
-    command, *args = ToyRobot::Command.process("PLACE 1, 2 NORTH")
-    expect(command).to eq(:place)
-    expect(args).to eq([1, 2, "NORTH"])
+    it "processes a PLACE command" do
+      command, *args = ToyRobot::Command.process("PLACE 1,2,NORTH") 
+      expect(command).to eq(:place)
+      expect(args).to eq([1, 2, "NORTH"])
+    end  
   end
 end
