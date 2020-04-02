01dd38fe5e2256a88a9c47d82b2e80aaa6434268
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-04-02 22:56:16 +1100

    Test checks ability to load commands from file

--- lib/toy_robot/cli.rb
+++ lib/toy_robot/cli.rb
@@ -1,4 +1,14 @@
 module ToyRobot
   class CLI
+
+    def load_commands(commands_file)
+      File.readlines(commands_file).map do |command|
+        ToyRobot::Command.process(command)
+      end
+    end
+
+    # def run(commands)
+    # end
+
   end
 end
